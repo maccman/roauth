218689fe9d81e734700c582e1dc73939137b9d8e
Philip (flip) Kromer <flip@infochimps.org>
2010-08-13 09:33:24 +0800

    Authorization header must have "quotes" around each parameter (http://oauth.net/core/1.0/#auth_header). Also, many providers demand the fields be sorted -- might as well make them happy.

--- lib/roauth.rb
+++ lib/roauth.rb
@@ -5,11 +5,11 @@ require "uri"
 module ROAuth
   class UnsupportedSignatureMethod < Exception; end
   class MissingOAuthParams < Exception; end
-  
+
   # Supported {signature methods}[http://oauth.net/core/1.0/#signing_process];
   SIGNATURE_METHODS = {"HMAC-SHA1" => OpenSSL::Digest::Digest.new("sha1")}
   OAUTH_PARAMS      = [:consumer_key, :token, :signature_method, :version, :nonce, :timestamp, :body_hash, :callback]
-  
+
   # Return an {OAuth "Authorization" HTTP header}[http://oauth.net/core/1.0/#auth_header] from request data
   def header(oauth, uri, params = {}, http_method = :get)
     oauth = oauth.dup
@@ -19,15 +19,17 @@ module ROAuth
     oauth[:timestamp]        ||= Time.now.to_i
     oauth[:token]            ||= oauth.delete(:access_key)
     oauth[:token_secret]     ||= oauth.delete(:access_secret)
-    
+
     sig_params = oauth_params(oauth)
     sig_params[:oauth_signature] = escape(
       signature(oauth, uri, sig_params.merge(params), http_method)
-    )
-    
-    %{OAuth } + sig_params.map {|key, value| [key, value].join("=") }.join(", ")
+      )
+    sorted_sig_params    = sig_params.sort_by{|k,v| [k.to_s, v.to_s] }
+    authorization_params = sorted_sig_params.map {|key, value| [key, "\"#{value}\""].join("=") }.join(", ")
+
+    %{OAuth } + authorization_params
   end
-  
+
   def parse(header)
     header = header.dup
     header = header.gsub!(/^OAuth\s/, "")
@@ -42,55 +44,55 @@ module ROAuth
     header[:access_key] = header[:token]
     header
   end
-  
+
   def verify(oauth, header, uri, params = {}, http_method = :get)
     header = header.is_a?(String) ? parse(header) : header.dup
-    
+
     client_signature = header.delete(:signature)
     oauth[:consumer_key]     ||= header[:consumer_key]
     oauth[:token]            ||= header[:token]
     oauth[:token_secret]     ||= oauth.delete(:access_secret)
     oauth[:signature_method] ||= "HMAC-SHA1"
     oauth[:version]          ||= "1.0"
-    
+
     sig_params = params.dup
     sig_params.merge!(oauth_params(header))
-    
+
     client_signature == signature(oauth, uri, sig_params, http_method)
   end
-  
+
   protected
     def oauth_params(oauth)
-      oauth = oauth.to_a.select {|key, value| 
-        OAUTH_PARAMS.include?(key) 
+      oauth = oauth.to_a.select {|key, value|
+        OAUTH_PARAMS.include?(key)
       }
-      oauth.inject({}) {|hash, (key, value)| 
+      oauth.inject({}) {|hash, (key, value)|
         hash["oauth_#{key}"] = escape(value)
         hash
       }
     end
-  
+
     def signature(oauth, uri, params, http_method = :get)
       uri = URI.parse(uri)
       uri.query = nil
       uri = uri.to_s
-      
+
       sig_base = http_method.to_s.upcase + "&" + escape(uri) + "&" + normalize(params)
       digest   = SIGNATURE_METHODS[oauth[:signature_method]]
       secret   = "#{escape(oauth[:consumer_secret])}&#{escape(oauth[:token_secret])}"
-      
+
       Base64.encode64(OpenSSL::HMAC.digest(digest, secret, sig_base)).chomp.gsub(/\n/, "")
     end
-  
+
     # Escape characters in a string according to the {OAuth spec}[http://oauth.net/core/1.0/]
     def escape(value)
       URI.escape(value.to_s, /[^a-zA-Z0-9\-\.\_\~]/) # Unreserved characters -- must not be encoded
     end
-    
+
     def unescape(value)
       URI.unescape(value)
     end
-  
+
     # Normalize a string of parameters based on the {OAuth spec}[http://oauth.net/core/1.0/#rfc.section.9.1.1]
     def normalize(params)
       # Stringify keys - so we can compare them
@@ -106,6 +108,6 @@ module ROAuth
           [escape(key), escape(values)] * "%3D"
         end
       end * "%26"
-    end    
+    end
   extend self
 end
